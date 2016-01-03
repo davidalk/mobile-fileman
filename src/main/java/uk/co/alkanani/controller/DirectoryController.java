3d88e62e4e202b2b4048e7a299f8b3aaef72b484
davidalk <david.alkanani@gmail.com>
2016-01-03 18:31:16 +0000

    directory utils

--- src/main/java/uk/co/alkanani/controller/DirectoryController.java
+++ src/main/java/uk/co/alkanani/controller/DirectoryController.java
@@ -3,9 +3,8 @@ package uk.co.alkanani.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import uk.co.alkanani.file.DirectoryUtil;
 import uk.co.alkanani.json.DirectoryJson;
-
-import java.util.Arrays;
 
 @RestController
 @RequestMapping(path = "/directories")
@@ -13,14 +12,6 @@ public class DirectoryController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public DirectoryJson getDirectories() {
-        DirectoryJson testRoot = new DirectoryJson();
-        DirectoryJson child1 = new DirectoryJson();
-        DirectoryJson child2 = new DirectoryJson();
-        DirectoryJson child3 = new DirectoryJson();
-        child1.name = "child_1";
-        child2.name = "child_2";
-        child3.name = "child_3";
-        testRoot.subDirectories = Arrays.asList(child1, child2, child3);
-        return testRoot;
+        return DirectoryUtil.buildDirectoryJsonTree();
     }
 }
