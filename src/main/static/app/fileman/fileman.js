135e2bda49434252b5a11d0c48b9690af56c5c0f
davidalk <david.alkanani@gmail.com>
2016-01-17 15:42:17 +0000

    cleanup

--- src/main/static/app/fileman/fileman.js
+++ src/main/static/app/fileman/fileman.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var $fileman = angular.module('myApp.fileman', ['ngRoute'])
+var fileman = angular.module('myApp.fileman', ['ngRoute'])
 
-$fileman.config(['$routeProvider', function ($routeProvider) {
+fileman.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/fileman', {
         templateUrl: 'fileman/fileman.html',
         controller: 'FilemanCtrl'
     });
 }]);
 
-$fileman.controller('FilemanCtrl', [function () {
+fileman.controller('FilemanCtrl', [function () {
 
 }]);
 
