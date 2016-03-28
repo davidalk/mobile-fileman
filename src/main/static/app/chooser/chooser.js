5c84c1d7bbc408f0c13019277c750e0026c181bd
davidalk <david.alkanani@gmail.com>
2016-03-28 15:30:16 +0100

    cleanup promise & remove close button

--- src/main/static/app/chooser/chooser.js
+++ src/main/static/app/chooser/chooser.js
@@ -6,9 +6,8 @@ angular.module('myApp.chooser', ['ui.bootstrap', 'ngMaterial'])
     .directive('directoryChooser', [
         '$mdSidenav',
         '$http',
-        '$q',
         '$log',
-        function ($mdSidenav, $http, $q, $log) {
+        function ($mdSidenav, $http, $log) {
             return {
                 restrict: 'E',
                 scope: {
@@ -28,17 +27,16 @@ angular.module('myApp.chooser', ['ui.bootstrap', 'ngMaterial'])
 
                 function getDirectories(parent) {
                     var url = '/webapp/directories/';
-                    var q = $q.defer();
 
                     if (parent) {
                         url += parent.replace(/^\//, '');
                     }
 
-                    $http.get(url)
+                    return $http.get(url)
                         .then(function success(response) {
                             $log.info('Directory service success');
                             //noinspection JSUnresolvedVariable
-                            q.resolve(response.data.subDirectories);
+                            return response.data.subDirectories;
                         }, function error(response) {
                             $log.error('Failed contacting directory service: ' + response);
                             if (!parent || parent === '/') {
@@ -48,7 +46,6 @@ angular.module('myApp.chooser', ['ui.bootstrap', 'ngMaterial'])
                             }
                         });
 
-                    return q.promise;
                 }
 
                 scope.open = function () {
@@ -56,14 +53,6 @@ angular.module('myApp.chooser', ['ui.bootstrap', 'ngMaterial'])
                         .open()
                         .then(function () {
                             $log.debug('open ' + scope.side);
-                        });
-                };
-
-                scope.close = function () {
-                    sidenavInstance
-                        .close()
-                        .then(function () {
-                            $log.debug('close ' + scope.side);
                         });
                 };
 
