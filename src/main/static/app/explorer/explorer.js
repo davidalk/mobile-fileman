96f65558f82ff0a1f4436dd864ee19b2a47a34b3
davidalk <david.alkanani@gmail.com>
2016-06-18 15:26:18 +0100

    setup for file icons

--- src/main/static/app/explorer/explorer.js
+++ src/main/static/app/explorer/explorer.js
@@ -21,6 +21,9 @@ angular.module('myApp.explorer', [])
                     tileWidth = 150, tileHeight = 175,
                     labelLength = parseInt(scope.labelLength), maxWidthForTiles = calcMaxWidthForTiles();
 
+                var directorySvgPath = 'M 118.222 24 h -62.222 v -6.222 c 0 -5.4 -4.378 -9.778 -9.778 -9.778 h -36.444 c -5.4 0 -9.778 4.378 -9.778 9.778 v 100.445 c 0 5.4 4.378 9.77801 9.778 9.77801 h 108.445 c 5.4 0 9.77801 -4.378 9.77801 -9.778 v -84.445 c 0 -5.4 -4.378 -9.778 -9.778 -9.778 Z';
+                var fileSvgPath = 'M17.778 0c-5.4 0-9.778 4.378-9.778 9.778v108.445c0 5.4 4.378 9.778 9.778 9.778h84.445c5.4 0 9.778-4.378 9.778-9.778v-86.222h-22.222c-5.4 0-9.778-4.378-9.778-9.778v-22.222h-62.222z';
+
                 var svg = d3.select(element[0])
                     .append('svg')
                     .attr('viewBox', '0 0 ' + svgWidth + ' ' + svgHeight)
@@ -48,55 +51,59 @@ angular.module('myApp.explorer', [])
                     var files = _.filter(data, {'isDirectory': false});
                     var directories = _.filter(data, {'isDirectory': true});
 
-                    var tiles = svg.selectAll(".directories")
+                    var directoryTiles = svg.selectAll(".directories")
                         .data(directories, function (d) {
                             return d.name;
                         });
 
-                    var g = tiles.enter().append('g')
+                    var g = directoryTiles.enter().append('g')
                         .attr('class', 'directories')
-                        .attr('transform', function (d, i) {
-                            var x, y, oneDimPos;
-
-                            maxWidthForTiles = calcMaxWidthForTiles();
-                            oneDimPos = i * tileWidth;
-
-                            if ((oneDimPos + tileWidth) > maxWidthForTiles) {
-                                x = oneDimPos % maxWidthForTiles;
-                                y = Number.parseInt((oneDimPos / maxWidthForTiles)) * tileHeight * 1.2;
-                            } else {
-                                x = oneDimPos;
-                                y = 0;
-                            }
-
-                            return 'translate(' + x + ',' + y + ')';
-                        });
+                        .attr('transform', gTranslate);
 
                     g.append('path')
-                        .attr('d', "M 118.222 24 h -62.222 v -6.222 c 0 -5.4 -4.378 -9.778 -9.778 -9.778 h -36.444 c -5.4 0 -9.778 4.378 -9.778 9.778 v 100.445 c 0 5.4 4.378 9.77801 9.778 9.77801 h 108.445 c 5.4 0 9.77801 -4.378 9.77801 -9.778 v -84.445 c 0 -5.4 -4.378 -9.778 -9.778 -9.778 Z")
+                        .attr('d', directorySvgPath)
                         .attr('stroke', 'black')
                         .attr('fill', 'dodgerblue')
                         .style({"vector-effect": "non-scaling-stroke", "stroke-width": "1px"});
 
                     g.append('text')
                         .attr('x', '0')
-                        .attr('y', function (d) {
-                            var name = d.name;
-                            if (name.length > labelLength) {
-                                var words = splitByDelim(name);
-                                var lineCount = words.length === 1 ? Math.ceil(name.length / labelLength) : words.length;
-                                return '-' + (lineCount * 1.2) + 'em';
-
-                            } else {
-                                return '-1.2em';
-                            }
-                        })
+                        .attr('y', textDeltaY)
                         .html(function (d) {
-                            return splitText(d.name);
+                            return generateTspans(d.name);
                         });
 
-                    tiles.exit().remove();
+                    directoryTiles.exit().remove();
 
+                }
+
+                function gTranslate(d, i) {
+                    var x, y, oneDimPos;
+
+                    maxWidthForTiles = calcMaxWidthForTiles();
+                    oneDimPos = i * tileWidth;
+
+                    if ((oneDimPos + tileWidth) > maxWidthForTiles) {
+                        x = oneDimPos % maxWidthForTiles;
+                        y = Number.parseInt((oneDimPos / maxWidthForTiles)) * tileHeight * 1.2;
+                    } else {
+                        x = oneDimPos;
+                        y = 0;
+                    }
+
+                    return 'translate(' + x + ',' + y + ')';
+                }
+
+                function textDeltaY(d) {
+                    var name = d.name;
+                    if (name.length > labelLength) {
+                        var words = splitByDelim(name);
+                        var lineCount = words.length === 1 ? Math.ceil(name.length / labelLength) : words.length;
+                        return '-' + (lineCount * 1.2) + 'em';
+
+                    } else {
+                        return '-1.2em';
+                    }
                 }
 
                 function calcMaxWidthForTiles() {
@@ -108,7 +115,7 @@ angular.module('myApp.explorer', [])
 
                 }
 
-                function splitText(text) {
+                function generateTspans(text) {
                     var words, i, j, newWord, result = [],
                         textLength = labelLength;
 
@@ -141,7 +148,7 @@ angular.module('myApp.explorer', [])
 
                 function splitByDelim(input) {
                     var delimiters = [' ', '-', '.'];
-                    for (var i=0; i<delimiters.length; i++) {
+                    for (var i = 0; i < delimiters.length; i++) {
                         if (input.includes(delimiters[i])) {
                             return _.map(input.split(delimiters[i]), function (d, index, collection) {
                                 return d + ((index < collection.length - 1) ? delimiters[i] : '');
@@ -150,6 +157,7 @@ angular.module('myApp.explorer', [])
                     }
                     return [input];
                 }
+
 
             }
 
